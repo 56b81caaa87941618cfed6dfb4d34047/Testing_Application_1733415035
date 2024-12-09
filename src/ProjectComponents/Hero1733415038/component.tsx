0f23d19c7179be1e863f6835bbe3e6913d4f35b8
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-12-09 11:13:29 +0000

    feat: Edit Hero1733415038
    Requested change: Align all content to the right

--- src/ProjectComponents/Hero1733415038/component.tsx
+++ src/ProjectComponents/Hero1733415038/component.tsx
@@ -4,8 +4,8 @@ const Hero: React.FC = () => {
   
   return (
     <div className="bg-red-500 py-16 text-white w-full h-full">
-      <div className="container mx-auto px-4 flex flex-col md:flex-row items-center h-full">
-        <div className="md:w-1/2 mb-8 md:mb-0">
+      <div className="container mx-auto px-4 flex flex-col md:flex-row justify-end items-center h-full">
+        <div className="md:w-1/2 text-right">
           <h1 className="text-4xl font-bold mb-4">Elevate Your Testing Experience</h1>
           <p className="text-xl mb-6">Streamline your QA process with our powerful and intuitive testing platform</p>
         </div>
